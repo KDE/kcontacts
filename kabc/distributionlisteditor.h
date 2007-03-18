dda8579e5b4f01eae754ebcd0bb758376d084bae
Allen Winter <winter@kde.org>
2007-03-18 00:32:49 +0000

    remove the obsolete DistributionListEditor and associated test program. next, to rename.
    svn path=/trunk/KDE/kdepimlibs/; revision=643626

--- kabc/distributionlisteditor.h
+++ kabc/distributionlisteditor.h
@@ -38,7 +38,7 @@ class KABC_EXPORT EmailSelectDialog : public KDialog
   public:
     EmailSelectDialog( const QStringList &emails, const QString &current,
                        QWidget *parent );
-    
+
     QString selected();
 
     static QString getEmail( const QStringList &emails, const QString &current,
@@ -46,39 +46,6 @@ class KABC_EXPORT EmailSelectDialog : public KDialog
 
   private:
     QButtonGroup *mButtonGroup;
-};
-
-/**
-  @obsolete
-*/
-class KABC_EXPORT DistributionListEditor : public QWidget
-{
-    Q_OBJECT
-  public:
-    DistributionListEditor( AddressBook *, QWidget *parent );
-    virtual ~DistributionListEditor();
-
-  private Q_SLOTS:
-    void newList();
-    void removeList();
-    void addEntry();
-    void removeEntry();
-    void changeEmail();
-    void updateEntryView();
-    void updateAddresseeView();
-    void updateNameCombo();
-    void slotSelectionEntryViewChanged();
-    void slotSelectionAddresseeViewChanged();
-
-  private:
-    QComboBox *mNameCombo;  
-    QTreeWidget *mEntryView;
-    QTreeWidget *mAddresseeView;
-
-    AddressBook *mAddressBook;
-    DistributionListManager *mManager;
-    QPushButton *newButton, *removeButton;
-    QPushButton *changeEmailButton,*removeEntryButton,*addEntryButton;
 };
 
 }
