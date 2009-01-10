0eeeb428d3512149c9f8aad9216582799c0fab0f
Tobias Koenig <tokoe@kde.org>
2009-01-10 09:05:18 +0000

    Extended auto tests
    svn path=/trunk/KDE/kdepimlibs/; revision=908700

--- kabc/tests/contactgrouptest.cpp
+++ kabc/tests/contactgrouptest.cpp
@@ -32,11 +32,202 @@ class ContactGroupTest : public QObject
   Q_OBJECT
 
   private Q_SLOTS:
+    void contactGroupReference();
+    void contactGroupData();
+    void contactGroup();
     void testGroupRoundTrip();
     void testGroupListRoundTrip();
 };
 
 QTEST_KDEMAIN( ContactGroupTest, NoGUI )
+
+void ContactGroupTest::contactGroupReference()
+{
+  const QString uid( "MyReferenceId" );
+  const QString preferredEMail( "tokoe@kde.org" );
+  const QString customKey( "MyCustomKey" );
+  const QString customValue( "MyCustomValue" );
+
+  // uid test
+  {
+    ContactGroup::Reference ref( uid );
+    QCOMPARE( ref.uid(), uid );
+  }
+
+  // uid test
+  {
+    ContactGroup::Reference ref;
+    ref.setUid( uid );
+    QCOMPARE( ref.uid(), uid );
+  }
+
+  // preferredEmail test
+  {
+    ContactGroup::Reference ref( uid );
+    ref.setPreferredEmail( preferredEMail );
+    QCOMPARE( ref.preferredEmail(), preferredEMail );
+  }
+
+  // custom test
+  {
+    ContactGroup::Reference ref( uid );
+    ref.insertCustom( customKey, customValue );
+    QCOMPARE( ref.custom( customKey ), customValue );
+    ref.removeCustom( customKey );
+    QCOMPARE( ref.custom( customKey ), QString() );
+  }
+
+  // assignment test
+  {
+    ContactGroup::Reference ref( uid );
+    ref.setPreferredEmail( preferredEMail );
+    ref.insertCustom( customKey, customValue );
+
+    ContactGroup::Reference ref2( ref );
+    QCOMPARE( ref.uid(), ref2.uid() );
+    QCOMPARE( ref.preferredEmail(), ref2.preferredEmail() );
+    QCOMPARE( ref.custom( customKey ), ref2.custom( customKey ) );
+
+    QVERIFY( ref == ref2 );
+  }
+
+  // const test
+  {
+    ContactGroup::Reference ref( uid );
+    ref.setPreferredEmail( preferredEMail );
+    ref.insertCustom( customKey, customValue );
+
+    const ContactGroup::Reference constRef( ref );
+    constRef.uid();
+    constRef.preferredEmail();
+    constRef.custom( customKey );
+  }
+}
+
+void ContactGroupTest::contactGroupData()
+{
+  const QString name( "Tobias Koenig" );
+  const QString email( "tokoe@kde.org" );
+  const QString customKey( "MyCustomKey" );
+  const QString customValue( "MyCustomValue" );
+
+  // name/email test
+  {
+    ContactGroup::Data data( name, email );
+    QCOMPARE( data.name(), name );
+    QCOMPARE( data.email(), email );
+  }
+
+  // name test
+  {
+    ContactGroup::Data data;
+    data.setName( name );
+    QCOMPARE( data.name(), name );
+  }
+
+  // email test
+  {
+    ContactGroup::Data data;
+    data.setEmail( email );
+    QCOMPARE( data.email(), email );
+  }
+
+  // custom test
+  {
+    ContactGroup::Data data( name, email );
+    data.insertCustom( customKey, customValue );
+    QCOMPARE( data.custom( customKey ), customValue );
+    data.removeCustom( customKey );
+    QCOMPARE( data.custom( customKey ), QString() );
+  }
+
+  // assignment test
+  {
+    ContactGroup::Data data( name, email );
+    data.insertCustom( customKey, customValue );
+
+    ContactGroup::Data data2( data );
+    QCOMPARE( data.name(), data2.name() );
+    QCOMPARE( data.email(), data2.email() );
+    QCOMPARE( data.custom( customKey ), data2.custom( customKey ) );
+
+    QVERIFY( data == data2 );
+  }
+
+  // const test
+  {
+    ContactGroup::Data data( name, email );
+    data.insertCustom( customKey, customValue );
+
+    const ContactGroup::Data constData( data );
+    constData.name();
+    constData.email();
+    constData.custom( customKey );
+  }
+}
+
+void ContactGroupTest::contactGroup()
+{
+  const QString groupName( "MyGroupName" );
+  const QString groupId( "MyGroupID" );
+  const QString name( "Tobias Koenig" );
+  const QString email( "tokoe@kde.org" );
+  const QString uid( "MyUid" );
+
+  // name test
+  {
+    ContactGroup group( groupName );
+    QCOMPARE( group.name(), groupName );
+  }
+
+  // id test
+  {
+    ContactGroup group( groupName );
+    group.setId( groupId );
+    QCOMPARE( group.id(), groupId );
+  }
+
+  // reference test
+  {
+    ContactGroup group( groupName );
+    QCOMPARE( group.referencesCount(), (unsigned int)0 );
+
+    ContactGroup::Reference ref( uid );
+    ref.setPreferredEmail( email );
+
+    group.append( ref );
+    QCOMPARE( group.referencesCount(), (unsigned int)1 );
+
+    const ContactGroup::Reference ref2 = group.reference( 0 );
+    QCOMPARE( ref, ref2 );
+
+    group.remove( ref );
+    QCOMPARE( group.referencesCount(), (unsigned int)0 );
+  }
+
+  // data test
+  {
+    ContactGroup group( groupName );
+    QCOMPARE( group.dataCount(), (unsigned int)0 );
+
+    ContactGroup::Data data( name, email );
+
+    group.append( data );
+    QCOMPARE( group.dataCount(), (unsigned int)1 );
+
+    const ContactGroup::Data data2 = group.data( 0 );
+    QCOMPARE( data, data2 );
+
+    group.remove( data );
+    QCOMPARE( group.dataCount(), (unsigned int)0 );
+  }
+
+  // mimetype test
+  {
+    ContactGroup group( groupName );
+    QCOMPARE( group.mimeType(), QLatin1String( "application/x-vnd.kde.contactgroup" ) );
+  }
+}
 
 void ContactGroupTest::testGroupRoundTrip()
 {
