2809a62cfb0be7353b8fdda441a9fe99e05d3f02
Kevin Krammer <kevin.krammer@gmx.at>
2010-05-03 09:02:20 +0000

    Add VCardDrag namespace for QMimeData based VCard transport. This is basically just a slightly improved and unittest covered version of libkdepim/kvcarddrag
    svn path=/trunk/KDE/kdepimlibs/; revision=1122177

---
+++ kabc/vcarddrag.cpp
@@ -0,0 +1,99 @@
+/*
+  This file is part of libkabc.
+
+  Copyright (c) 2002 Tobias Koenig <tokoe@kde.org>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "vcarddrag.h"
+
+#include "vcardconverter.h"
+
+#include <KMimeType>
+
+#include <QMimeData>
+
+using namespace KABC;
+
+static QString findCompatibleMimeType( const QMimeData *md )
+{
+  // check the canonical MIME type first
+  if ( md->hasFormat( VCardDrag::mimeType() ) ) {
+    return VCardDrag::mimeType();
+  }
+
+  const QStringList mimeTypeOffers = md->formats();
+  Q_FOREACH( const QString &mimeType, mimeTypeOffers ) {
+   const KMimeType::Ptr mimeTypePtr = KMimeType::mimeType( mimeType, KMimeType::ResolveAliases );
+    if ( !mimeTypePtr.isNull() ) {
+      if ( mimeTypePtr->is( VCardDrag::mimeType() ) ) {
+        return mimeType;
+      }
+    }
+  }
+
+  return QString();
+}
+
+QString VCardDrag::mimeType()
+{
+  return Addressee::mimeType();
+}
+
+bool VCardDrag::populateMimeData( QMimeData *md, const QByteArray &content )
+{
+  md->setData( mimeType(), content );
+  return true;
+}
+
+bool VCardDrag::populateMimeData( QMimeData *md, const KABC::Addressee::List &addressees )
+{
+  KABC::VCardConverter converter;
+  const QByteArray vcards = converter.createVCards( addressees );
+  if ( !vcards.isEmpty() ) {
+    return populateMimeData( md, vcards );
+  } else {
+    return false;
+  }
+}
+
+bool VCardDrag::canDecode( const QMimeData *md )
+{
+  return !findCompatibleMimeType( md ).isEmpty();
+}
+
+bool VCardDrag::fromMimeData( const QMimeData *md, QByteArray &content )
+{
+  const QString mimeOffer = findCompatibleMimeType( md );
+  if ( mimeOffer.isEmpty() ) {
+    return false;
+  }
+  content = md->data( mimeOffer );
+  return !content.isEmpty();
+}
+
+bool VCardDrag::fromMimeData( const QMimeData *md, KABC::Addressee::List &addressees )
+{
+  const QString mimeOffer = findCompatibleMimeType( md );
+  if ( mimeOffer.isEmpty() ) {
+    return false;
+  }
+  addressees = KABC::VCardConverter().parseVCards( md->data( mimeOffer ) );
+  return !addressees.isEmpty();
+}
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
---
+++ kabc/vcarddrag.h
@@ -0,0 +1,109 @@
+/*
+  This file is part of libkabc.
+
+  Copyright (c) 2002 Tobias Koenig <tokoe@kde.org>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KABC_VCARDDRAG_H
+#define KABC_VCARDDRAG_H
+
+#include "kabc_export.h"
+
+#include <KABC/Addressee>
+
+namespace KABC {
+
+/**
+  @brief Utility function for implementing drag&drop for vCards
+
+  See the Qt drag'n'drop documentation.
+
+  @since 4.5
+*/
+namespace VCardDrag
+{
+  /**
+    Mime-type of vCard.
+
+    @see Addressee::mimeType()
+
+    @return the MIME type used for vcards: @c text/directory
+  */
+  KABC_EXPORT QString mimeType();
+
+  /**
+    Adds the vCard representation as data of the drag object.
+
+    @param md the object to set the data on
+    @param content the vCard data to set
+
+    @return Always @c true
+  */
+  KABC_EXPORT bool populateMimeData( QMimeData *md, const QByteArray &content );
+
+  /**
+    Adds the vCard representation as data of the drag object.
+
+    @param md the object to set the data on
+    @param addressees list of Addressee objects to serialize to vCard
+
+    @return @c true if serializing of the given list worked, otherwise @c false
+
+    @see VCardConverter::createVCards()
+  */
+  KABC_EXPORT bool populateMimeData( QMimeData *md, const KABC::Addressee::List &addressees );
+
+  /**
+    Returns if drag&drop object can be decoded to vCard.
+
+    @param md the object to check for vCard data
+
+    @return @c true if the given data object contains a vCard MIME type, otherwise @c false
+  */
+  KABC_EXPORT bool canDecode( const QMimeData *md );
+
+  /**
+    Decodes the drag&drop object to vCard component @p content.
+
+    @param md the object to check for vCard data
+    @param content where to put the vCard data from @p md
+
+    @return @c true if there was data for the vCard MIME type, otherwise @c false
+
+    @see canDecode()
+  */
+  KABC_EXPORT bool fromMimeData( const QMimeData *md, QByteArray &content );
+
+  /**
+    Decodes the MIME data @p md and puts the resulting vCard into @p addresseess.
+
+    @param md the object to check for vCard data
+    @param addressees where to put the parsed vCards from @p md
+
+    @return @c true if there was data for the vCard MIME type and it could be parsed successfully,
+            otherwise @c false
+
+    @see canDecode()
+  */
+  KABC_EXPORT bool fromMimeData( const QMimeData *md, KABC::Addressee::List &addressees );
+}
+
+}
+
+#endif // VCARDDRAG_H
+// kate: space-indent on; indent-width 2; replace-tabs on;
