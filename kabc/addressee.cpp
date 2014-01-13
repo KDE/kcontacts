82937926066676c8cf93a30b3fdeb485ff38175a
Christian Mollekopf <chrigi_1@fastmail.fm>
2014-01-13 22:31:13 +0100

    Make generated UUID string RFC4122 compliant.
    This ensures the uid can be used directly in urn's like:
    urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6
    
    By default the uuid string contains curly braces:
    {f81d4fae-7dec-11d0-a765-00a0c91e6bf6}

--- kabc/addressee.cpp
+++ kabc/addressee.cpp
@@ -51,7 +51,7 @@ class Addressee::Private : public QSharedData
 {
   public:
     Private()
-      : mUid( QUuid::createUuid().toString() ),
+      : mUid( QUuid::createUuid().toString().mid(1, 36) ), //We avoid the curly braces so the string is RFC4122 compliant and can be used as urn
 #ifndef KDEPIM_NO_KRESOURCES
       mResource( 0 ),
 #endif
