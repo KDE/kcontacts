8eb4370108c6638df0c4100df4107500f22dd5e1
Montel Laurent <montel@kde.org>
2015-07-19 07:34:58 +0200

    Add resource locator support (vcard4)

---
+++ src/resourcelocatorurl.cpp
@@ -0,0 +1,133 @@
+/*
+    This file is part of the KContacts framework.
+    Copyright (c) 2015 Laurent Montel <montel@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "resourcelocatorurl.h"
+#include <QMap>
+#include <QUrl>
+#include <QDataStream>
+#include <qstringlist.h>
+
+using namespace KContacts;
+
+class ResourceLocatorUrl::Private : public QSharedData
+{
+public:
+    Private()
+    {
+    }
+
+    Private(const Private &other)
+        : QSharedData(other)
+    {
+        parameters = other.parameters;
+        url = other.url;
+    }
+    QMap<QString, QStringList> parameters;
+    QUrl url;
+};
+
+ResourceLocatorUrl::ResourceLocatorUrl()
+    : d(new Private)
+{
+}
+
+ResourceLocatorUrl::ResourceLocatorUrl(const ResourceLocatorUrl &other)
+    : d(other.d)
+{
+}
+
+ResourceLocatorUrl::~ResourceLocatorUrl()
+{
+
+}
+
+QMap<QString, QStringList> ResourceLocatorUrl::parameters() const
+{
+    return d->parameters;
+}
+
+bool ResourceLocatorUrl::operator==(const ResourceLocatorUrl &other) const
+{
+    return (d->parameters == other.parameters()) && (d->url == other.url());
+}
+
+bool ResourceLocatorUrl::operator!=(const ResourceLocatorUrl &other) const
+{
+    return !(other == *this);
+}
+
+ResourceLocatorUrl &ResourceLocatorUrl::operator=(const ResourceLocatorUrl &other)
+{
+    if (this != &other) {
+        d = other.d;
+    }
+
+    return *this;
+}
+
+QString ResourceLocatorUrl::toString() const
+{
+    QString str;
+    str += QString::fromLatin1("ResourceLocatorUrl {\n");
+    str += QString::fromLatin1("    url: %1\n").arg(d->url.toString());
+    if (!d->parameters.isEmpty()) {
+        QMapIterator<QString, QStringList> i(d->parameters);
+        QString param;
+        while (i.hasNext()) {
+            i.next();
+            param += QString::fromLatin1("%1 %2").arg(i.key()).arg(i.value().join(QStringLiteral(",")));
+        }
+        str += QString::fromLatin1("    parameters: %1\n").arg(param);
+    }
+    str += QString::fromLatin1("}\n");
+    return str;
+}
+
+void ResourceLocatorUrl::setParameters(const QMap<QString, QStringList> &params)
+{
+    d->parameters = params;
+}
+
+bool ResourceLocatorUrl::isValid() const
+{
+    return (d->url.isValid());
+}
+
+void ResourceLocatorUrl::setUrl(const QUrl &url)
+{
+    d->url = url;
+}
+
+QUrl ResourceLocatorUrl::url() const
+{
+    return d->url;
+}
+
+QDataStream &KContacts::operator<<(QDataStream &s, const ResourceLocatorUrl &calUrl)
+{
+    return s << calUrl.d->parameters << calUrl.d->url;
+}
+
+QDataStream &KContacts::operator>>(QDataStream &s, ResourceLocatorUrl &calUrl)
+{
+    s >> calUrl.d->parameters >> calUrl.d->url;
+    return s;
+}
+
---
+++ src/resourcelocatorurl.h
@@ -0,0 +1,76 @@
+/*
+    This file is part of the KContacts framework.
+    Copyright (c) 2015 Laurent Montel <montel@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef RESOURCELOCATORURL_H
+#define RESOURCELOCATORURL_H
+
+#include "kcontacts_export.h"
+
+#include <QtCore/QSharedDataPointer>
+#include <QtCore/QString>
+#include <QtCore/QMap>
+class QUrl;
+/** @short Class that holds a Resource Locator
+ *  @since 5.0
+ */
+
+namespace KContacts
+{
+
+class KCONTACTS_EXPORT ResourceLocatorUrl
+{
+    friend KCONTACTS_EXPORT QDataStream &operator<<(QDataStream &, const ResourceLocatorUrl &);
+    friend KCONTACTS_EXPORT QDataStream &operator>>(QDataStream &, ResourceLocatorUrl &);
+public:
+
+    ResourceLocatorUrl();
+    ResourceLocatorUrl(const ResourceLocatorUrl &other);
+
+    ~ResourceLocatorUrl();
+
+    typedef QVector<ResourceLocatorUrl> List;
+
+    bool isValid() const;
+
+    void setUrl(const QUrl &url);
+    QUrl url() const;
+
+    void setParameters(const QMap<QString, QStringList> &params);
+    QMap<QString, QStringList> parameters() const;
+
+    bool operator==(const ResourceLocatorUrl &other) const;
+    bool operator!=(const ResourceLocatorUrl &other) const;
+
+    ResourceLocatorUrl &operator=(const ResourceLocatorUrl &other);
+
+    QString toString() const;
+private:
+    class Private;
+    QSharedDataPointer<Private> d;
+};
+
+KCONTACTS_EXPORT QDataStream &operator<<(QDataStream &stream, const ResourceLocatorUrl &object);
+
+KCONTACTS_EXPORT QDataStream &operator>>(QDataStream &stream, ResourceLocatorUrl &object);
+
+}
+Q_DECLARE_TYPEINFO( KContacts::ResourceLocatorUrl, Q_MOVABLE_TYPE );
+
+#endif // RESOURCELOCATORURL_H
