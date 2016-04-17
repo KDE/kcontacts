c84bddf0bf0d2ed8f100a45f32ecbbc4419d0a9f
Montel Laurent <montel@kde.org>
2016-04-17 14:05:22 +0200

    update copyright

--- src/resourcelocatorurl.cpp
+++ src/resourcelocatorurl.cpp
@@ -1,6 +1,6 @@
 /*
     This file is part of the KContacts framework.
-    Copyright (c) 2015 Laurent Montel <montel@kde.org>
+    Copyright (c) 2015-2016 Laurent Montel <montel@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
--- src/vcardtool.cpp
+++ src/vcardtool.cpp
@@ -1,7 +1,7 @@
 /*
     This file is part of the KContacts framework.
     Copyright (c) 2003 Tobias Koenig <tokoe@kde.org>
-    Copyright (c) 2015 Laurent Montel <montel@kde.org>
+    Copyright (c) 2015-2016 Laurent Montel <montel@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
