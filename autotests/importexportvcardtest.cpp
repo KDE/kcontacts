af64b2bf2c2e39fb1df0d8f74a459e7dae9ccb2a
Montel Laurent <montel@kde.org>
2016-07-14 08:55:54 +0200

    Add new autotests

---
+++ autotests/importexportvcardtest.cpp
@@ -0,0 +1,35 @@
+/*
+    This file is part of the KContacts framework.
+    Copyright (c) 2016 Laurent Montel <montel@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "importexportvcardtest.h"
+#include <QTest>
+
+ImportExportVCardTest::ImportExportVCardTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+ImportExportVCardTest::~ImportExportVCardTest()
+{
+
+}
+
+QTEST_MAIN(ImportExportVCardTest)
