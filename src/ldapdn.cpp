acbf548662eb59813a25fc787ccd9f43c5f24316
Montel Laurent <montel@kde.org>
2017-07-30 09:01:33 +0200

    Minor optimization

--- src/ldapdn.cpp
+++ src/ldapdn.cpp
@@ -55,7 +55,7 @@ bool LdapDN::LdapDNPrivate::isValidRDNString(const QString &rdn) const
     }
 
     // Split the rdn into the attribute name and value parts
-    const QStringList components = rdn.split(QLatin1Char('='));
+    const QVector<QStringRef> components = rdn.splitRef(QLatin1Char('='));
 
     // We should have exactly two parts
     if (components.size() != 2) {
